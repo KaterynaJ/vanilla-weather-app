79a9645e2f4ccf7a5dc718018cab9f4a5dd5261a
KaterynaJ <114310883+KaterynaJ@users.noreply.github.com>
2023-11-14 13:56:56 -0700

    converting temperature F to C and back
    Added links to temperature units that allow to convert temperature F to C and back.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
+
+//takes typed city name and triggers search function with it
 function handleSubmit (event) {
     event.preventDefault();
     let searchFieldValue = document.querySelector("#city-input-search");
     search(searchFieldValue.value);
 }
 
+//forms a request to weather website using api key, api url and variable that stores city name
 function search(city) {
     let apiKey = "3dc39916f235285875a7d4fd53778f07";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -15,7 +18,7 @@ function search(city) {
 }
 
 
-
+//retrieves current time and date on computer and formats it
 function formatDate(timestamp) {
     let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -36,9 +39,8 @@ function formatDate(timestamp) {
 }
 
 
-
+//retrieves weather data from website and changes html elements accordingly
 function displayTemperature (response) {
-    console.log(response);
 
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -71,3 +73,40 @@ function displayTemperature (response) {
 }
 
 
+//convert temp degree from C to F
+
+function showFahrenheitTemp(event) {
+    event.preventDefault();
+
+    let temperatureElement = document.querySelector("#temperature");
+
+    let celsiusTemp = parseInt(temperatureElement.innerHTML);
+
+    let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+
+    temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+
+}
+
+
+let fahrenheitLink = document.querySelector("#fahrenheit-units");
+fahrenheitLink.addEventListener("click", showFahrenheitTemp);   
+
+//convert temp degree fro F to C
+
+
+function showCelsiusTemp(event) {
+    event.preventDefault();
+
+    let temperatureElement = document.querySelector("#temperature");
+
+    let fahrenheitTemp = parseInt(temperatureElement.innerHTML);
+
+    let celsiusTemp = (fahrenheitTemp - 32) * 5/9;
+
+    temperatureElement.innerHTML = Math.round(celsiusTemp);
+
+}
+
+let celsiusLink = document.querySelector("#celsius-units");
+celsiusLink.addEventListener("click", showCelsiusTemp); 
